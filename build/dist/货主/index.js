1bf515a6245b782ce6c9d3087c0b0045ead6069c
Henry <liaohengyi@outlook.com>
2018-04-14 23:57:58 +0800

    deploy

--- build/dist/货主/index.js
+++
@@ -1,7 +0,0 @@
-import { mapperContainer as tuidMapperContainer } from './tuid';
-//import {mapperContainer as sheetMapperContainer} from './sheet';
-//import {mapperContainer as historyMapperContainer} from './history';
-export var pageMapper = {
-    tuid: tuidMapperContainer,
-};
-//# sourceMappingURL=index.js.map
